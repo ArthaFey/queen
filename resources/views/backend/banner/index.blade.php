756075f18c18d2aa4bfe9d794110930c1bb003d9
ArthaFey <feyartha2021@gmail.com>
2025-08-11 10:38:09 +0800

    update

---
+++ resources/views/backend/banner/index.blade.php
@@ -0,0 +1,36 @@
+@extends('backend.template.index')
+@section('content')
+
+<div class="container">
+ <div class="card mt-4">
+    <div class="card-header">
+    <h3 class="card-title">Banner</h3>
+    </div>
+    <!-- /.card-header -->
+    <div class="card-body p-0">
+    <table class="table">
+        <thead>
+        <tr>
+            <th style="width: 10px">#</th>
+            <th>Task</th>
+            <th>Progress</th>
+            <th style="width: 40px">Label</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr class="align-middle">
+            <td>1.</td>
+            <td>Update software</td>
+            <td></td>
+            <td><span class="badge text-bg-danger">55%</span></td>
+        </tr>
+        </tbody>
+    </table>
+    </div>
+    <!-- /.card-body -->
+</div>
+
+</div>
+
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,14 @@
 <?php
 
+use App\Http\Controllers\BannerController;
 use App\Http\Controllers\FrontEndController;
 use Illuminate\Support\Facades\Route;
 
+// ## HOMEPAGE ## //
 Route::get('/',[FrontEndController::class,'home'])->name('frontend.home');
+
+
+
+// ## BANNER ## //
+Route::get('/banner',[BannerController::class,'index'])->name('banner');
+
